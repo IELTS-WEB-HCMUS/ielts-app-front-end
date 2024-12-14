2c197007fba85df785958b9a89cb3fe134f739a2
MaThanhNhi <mathanhnhi2004@gmail.com>
2024-12-14 19:16:02 +0700

    Add Click event

---
+++ public/scripts/forgotPassPage.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('btn_submit_email').addEventListener('click', function() {
+        window.location.href = 'views/verify_otp_code_page.html';
+    });
+});
